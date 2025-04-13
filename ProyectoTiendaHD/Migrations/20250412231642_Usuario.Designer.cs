8d647fa08abfa9cf15aeaf4c1a5aad088e4de500
david-o-rosario1201 <finnnn023@gmail.com>
2025-04-13 00:06:55 -0400

    Crea el modelo usuarios

--- ProyectoTiendaHD/Migrations/20250412231642_Usuario.Designer.cs
+++ ProyectoTiendaHD/Migrations/20250412231642_Usuario.Designer.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ProyectoTiendaHD.DAL;
 
@@ -10,9 +11,11 @@ using ProyectoTiendaHD.DAL;
 namespace ProyectoTiendaHD.Migrations
 {
     [DbContext(typeof(Contexto))]
-    partial class ContextoModelSnapshot : ModelSnapshot
+    [Migration("20250412231642_Usuario")]
+    partial class Usuario
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ ProyectoTiendaHD/Modelos/Usuario.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProyectoTiendaHD.Modelos;
+
+public class Usuario
+{
+	[Key]
+	public int UsuarioId { get; set; }
+
+	public string Nombre { get; set; }
+}
