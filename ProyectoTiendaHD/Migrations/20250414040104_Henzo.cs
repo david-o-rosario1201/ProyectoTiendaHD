e82a67449fee9932c774b63d66a522baa2e811e5
HenzoWTF <Urenahenzo@gmail.com>
2025-04-14 21:54:07 -0400

    Listo

---
+++ ProyectoTiendaHD/Migrations/20250414040104_Henzo.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ProyectoTiendaHD.Migrations
+{
+    /// <inheritdoc />
+    public partial class Henzo : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
